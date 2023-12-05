945f2366804a88ede814ece94ac27c359ee056bc
Martina Řezníčková <reznickova.mart@gmail.com>
2023-12-05 21:35:05 +0100

    Final edits

--- ASP.NET_Core_MVC_Calculator/obj/Release/net8.0/ASP.NET_Core_MVC_Calculator.AssemblyInfo.cs
+++ ASP.NET_Core_MVC_Calculator/obj/Release/net8.0/ASP.NET_Core_MVC_Calculator.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASP.NET_Core_MVC_Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+293f9737371841c5f847759904213a612a25681a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+552a4e208664b667523762ae24427e67b9a92e75")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASP.NET_Core_MVC_Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASP.NET_Core_MVC_Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
